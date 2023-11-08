ea8d2240b83415c28577751a47bdf2eb09c3a21b
DaitokuAmy <daitoku1988@gmail.com>
2023-11-09 01:50:48 +0900

    DisposableHandleの追加

---
+++ Packages/com.daitokuamy.gameframework/Core/DisposableHandle.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace GameFramework.Core {
+    /// <summary>
+    /// DisposableHandleで管理するインスタンスのインターフェース
+    /// </summary>
+    public interface IDisposableHandleContent : IDisposable {
+        /// <summary>有効か</summary>
+        bool IsValid { get; }
+        /// <summary>完了しているか</summary>
+        bool IsDisposed { get; }
+    }
+    
+    /// <summary>
+    /// Disposableを管理するHandle
+    /// </summary>
+    public struct DisposableHandle : IDisposable {
+        /// <summary>有効なハンドルか</summary>
+        public bool IsValid => _content?.IsValid ?? false;
+        /// <summary>廃棄済みか</summary>
+        public bool IsDisposed => _content?.IsDisposed ?? true;
+
+        private IDisposableHandleContent _content;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="content">ハンドル管理するインスタンスのインターフェース</param>
+        public DisposableHandle(IDisposableHandleContent content) {
+            _content = content;
+        }
+
+        /// <summary>
+        /// 廃棄処理
+        /// </summary>
+        public void Dispose() {
+            if (!IsValid || IsDisposed) {
+                return;
+            }
+            
+            _content.Dispose();
+        }
+    }
+}
