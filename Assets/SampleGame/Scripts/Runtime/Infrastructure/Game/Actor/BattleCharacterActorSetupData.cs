ce18cbd33b243374ab9eaeeaa1df6bc58445affd
hironori_ito <daitoku1988@gmail.com>
2025-01-20 01:38:26 +0900

    サンプル用キャラモーション追加

--- Assets/SampleGame/Scripts/Runtime/Infrastructure/Game/Actor/BattleCharacterActorSetupData.cs
+++ Assets/SampleGame/Scripts/Runtime/Infrastructure/Game/Actor/BattleCharacterActorSetupData.cs
@@ -4,7 +4,7 @@ namespace SampleGame.Presentation {
     /// <summary>
     /// 戦闘用キャラアクター初期化データ
     /// </summary>
-    [CreateAssetMenu(menuName = "SampleGame/Actor Setup/Game Character", fileName = "dat_setup_battle_character_actor_ch000_00.asset")]
+    [CreateAssetMenu(menuName = "SampleGame/Actor Setup/Battle Character", fileName = "dat_setup_battle_character_actor_ch000_00.asset")]
     public class BattleCharacterActorSetupData : CharacterActorSetupData {
     }
 }
