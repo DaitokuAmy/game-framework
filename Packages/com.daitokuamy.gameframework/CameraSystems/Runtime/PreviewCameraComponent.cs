6530d8980a43ba612a5659d8ffb10053476f6507
hironori_ito <daitoku1988@gmail.com>
2025-02-02 02:34:27 +0900

    フォルダ構成の変更

---
+++ Packages/com.daitokuamy.gameframework/CameraSystems/Runtime/PreviewCameraComponent.cs
@@ -0,0 +1,86 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+namespace GameFramework.CameraSystems {
+    /// <summary>
+    /// ビューワー用のカメラコンポーネント
+    /// </summary>
+    public class PreviewCameraComponent : SerializedCameraComponent<CinemachineVirtualCamera> {
+        [SerializeField, Tooltip("距離")]
+        private float _distance = 10.0f;
+        [SerializeField, Tooltip("注視点オフセット")]
+        private Vector3 _lookAtOffset;
+        [SerializeField, Tooltip("X軸回転"), Range(-89.9f, 89.9f)]
+        private float _angleX;
+        [SerializeField, Tooltip("Y軸回転"), Range(0.0f, 360.0f)]
+        private float _angleY;
+        
+        // FOV
+        public float Fov {
+            get => VirtualCamera.m_Lens.FieldOfView;
+            set => VirtualCamera.m_Lens.FieldOfView = value;
+        }
+        // 距離
+        public float Distance {
+            get => _distance;
+            set => _distance = Mathf.Max(0.01f, value);
+        }
+        // X角度
+        public float AngleX {
+            get => _angleX;
+            set => _angleX = Mathf.Clamp(value, -89.9f, 89.9f);
+        }
+        // Y角度
+        public float AngleY {
+            get => _angleY;
+            set => _angleY = Mathf.Repeat(value, 360.0f);
+        }
+        // 注視対象
+        public Transform LookAt {
+            get => VirtualCamera.LookAt;
+            set => VirtualCamera.LookAt = value;
+        }
+        // 注視点オフセット
+        public Vector3 LookAtOffset {
+            get => _lookAtOffset;
+            set => _lookAtOffset = value;
+        }
+        // ニアクリップ
+        public float NearClip {
+            get => VirtualCamera.m_Lens.NearClipPlane;
+            set => VirtualCamera.m_Lens.NearClipPlane = Mathf.Clamp(value, 0.01f, FarClip);
+        }
+        // ファークリップ
+        public float FarClip {
+            get => VirtualCamera.m_Lens.FarClipPlane;
+            set => VirtualCamera.m_Lens.FarClipPlane = Mathf.Clamp(value, NearClip, float.MaxValue);
+        }
+
+        /// <summary>
+        /// 初期化処理
+        /// </summary>
+        protected override void InitializeInternal() {
+            // 基本的なコンポーネントは削除する
+            VirtualCamera.DestroyCinemachineComponent<CinemachineComponentBase>();
+        }
+
+        /// <summary>
+        /// カメラ更新処理
+        /// </summary>
+        protected override void UpdateInternal(float deltaTime) {
+            var trans = VirtualCamera.transform;
+            var lookAt = VirtualCamera.LookAt;
+            
+            // 相対位置計算
+            var relativePosition = Quaternion.Euler(_angleX, _angleY, 0.0f) * Vector3.back * _distance;
+
+            // LookAtを元に位置を計算
+            var basePosition = lookAt != null ? lookAt.position : Vector3.zero;
+            basePosition += Quaternion.Euler(0.0f, _angleY, 0.0f) * _lookAtOffset;
+            
+            // 姿勢に反映
+            trans.position = basePosition + relativePosition;
+            trans.LookAt(basePosition);
+        }
+    }
+}
---
+++ Packages/com.daitokuamy.gameframework/CameraSystems/Runtime/SerializedCameraComponent.cs
@@ -0,0 +1,145 @@
+using Unity.Cinemachine;
+using UnityEngine;
+using System;
+
+namespace GameFramework.CameraSystems {
+    /// <summary>
+    /// シリアライズ想定のカメラコンポーネント
+    /// </summary>
+    public abstract class SerializedCameraComponent<TCamera> : MonoBehaviour, ICameraComponent
+        where TCamera : CinemachineVirtualCameraBase {
+        [SerializeField, Tooltip("制御対象の仮想カメラ")]
+        private TCamera _virtualCamera;
+
+        private int _defaultPriority;
+
+        // アクティブ状態
+        bool ICameraComponent.IsActive => gameObject.activeSelf;
+
+        // 基本カメラ
+        ICinemachineCamera ICameraComponent.BaseCamera => _virtualCamera;
+        // 仮想カメラ
+        protected TCamera VirtualCamera => _virtualCamera;
+        
+        /// <summary>
+        /// 初期化処理
+        /// </summary>
+        void ICameraComponent.Initialize(CameraManager cameraManager) {
+            // Target指定が文字列で行われていたら取り直す
+            var cameraTarget = VirtualCamera.GetComponent<CameraTarget>();
+            if (cameraTarget != null) {
+                cameraTarget.SetupCameraTarget(cameraManager, VirtualCamera);
+            }
+            
+            // 初期化時点でのPriorityをキャッシュ
+            if (_virtualCamera != null) {
+                _defaultPriority = _virtualCamera.Priority;
+            }
+            
+            InitializeInternal();
+        }
+
+        /// <summary>
+        /// 廃棄時処理
+        /// </summary>
+        void IDisposable.Dispose() {
+            DisposeInternal();
+        }
+        
+        /// <summary>
+        /// カメラアクティブ時処理
+        /// カメラアクティブ時処理
+        /// </summary>
+        void ICameraComponent.Activate() {
+            if (_virtualCamera == null) {
+                return;
+            }
+
+            if (((ICameraComponent)this).IsActive) {
+                return;
+            }
+
+            gameObject.SetActive(true);
+            ActivateInternal();
+        }
+
+        /// <summary>
+        /// カメラ非アクティブ時処理
+        /// </summary>
+        void ICameraComponent.Deactivate() {
+            if (_virtualCamera == null) {
+                return;
+            }
+
+            if (!((ICameraComponent)this).IsActive) {
+                return;
+            }
+            
+            DeactivateInternal();
+            gameObject.SetActive(false);
+        }
+
+        /// <summary>
+        /// カメラ更新処理
+        /// </summary>
+        void ICameraComponent.Update(float deltaTime) {
+            if (_virtualCamera == null) {
+                return;
+            }
+            
+            UpdateInternal(deltaTime);
+        }
+
+        /// <summary>
+        /// プライオリティのセット(上書き用)
+        /// </summary>
+        void ICameraComponent.SetPriority(int priority) {
+            if (_virtualCamera == null) {
+                return;
+            }
+
+            _virtualCamera.Priority = priority;
+        }
+
+        /// <summary>
+        /// プライオリティのリセット
+        /// </summary>
+        void ICameraComponent.ResetPriority() {
+            if (_virtualCamera == null) {
+                return;
+            }
+
+            _virtualCamera.Priority = _defaultPriority;
+        }
+
+        /// <summary>
+        /// 初期化処理
+        /// </summary>
+        protected virtual void InitializeInternal() {
+        }
+
+        /// <summary>
+        /// 廃棄時処理
+        /// </summary>
+        protected virtual void DisposeInternal() {
+        }
+
+        /// <summary>
+        /// アクティブ時処理
+        /// </summary>
+        protected virtual void ActivateInternal() {
+        }
+        
+        /// <summary>
+        /// 非アクティブ時処理
+        /// </summary>
+        protected virtual void DeactivateInternal() {
+        }
+        
+        /// <summary>
+        /// 更新処理
+        /// </summary>
+        protected virtual void UpdateInternal(float deltaTime) {
+        }
+    }
+}
