ea3e435a8e7d84333d9695187fe07dbfc76b73a0
DaitokuAmy <daitoku1988@gmail.com>
2025-06-21 13:05:56 +0900

    TouchAnimationのパス貫通機能をオプションにして、内容を修正

--- Packages/com.daitokuamy.gameframework/Scripts/Runtime/UISystems/EventSystemUtils.cs
+++ Packages/com.daitokuamy.gameframework/Scripts/Runtime/UISystems/EventSystemUtils.cs
@@ -12,18 +12,27 @@ namespace GameFramework.UISystems {
         /// <summary>
         /// イベント貫通処理
         /// </summary>
-        public static void PassEvent<T>(GameObject source, PointerEventData data, ExecuteEvents.EventFunction<T> function)
+        public static void PassEvent<T>(PointerEventData data, ExecuteEvents.EventFunction<T> function)
             where T : IEventSystemHandler {
             s_raycastResults.Clear();
             EventSystem.current.RaycastAll(data, s_raycastResults);
             var current = data.pointerCurrentRaycast.gameObject;
+            var currentDepth = data.pointerCurrentRaycast.depth;
+            var currentIndex = current.transform.GetSiblingIndex();
+
             for (var i = 0; i < s_raycastResults.Count; i++) {
-                var target = s_raycastResults[i].gameObject;
-                if (target != current && target != source) {
-                    if (target.GetComponent<T>() != null) {
-                        ExecuteEvents.Execute(s_raycastResults[i].gameObject, data, function);
-                        break;
-                    }
+                var result = s_raycastResults[i];
+                if (result.depth > currentDepth) {
+                    continue;
+                }
+
+                if (result.gameObject.transform.GetSiblingIndex() >= currentIndex) {
+                    continue;
+                }
+
+                if (result.gameObject.GetComponent<T>() != null) {
+                    ExecuteEvents.Execute(result.gameObject, data, function);
+                    break;
                 }
             }
         }
