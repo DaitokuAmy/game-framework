2ab8c63650974bab07a326b52b08b53fb45226fc
DaitokuAmy <daitoku1988@gmail.com>
2025-08-07 20:17:48 +0900

    LogのDefinitionを定数化

--- Packages/com.daitokuamy.gameframework/Scripts/Runtime/Common/Log/DebugLog.cs
+++ Packages/com.daitokuamy.gameframework/Scripts/Runtime/Common/Log/DebugLog.cs
@@ -10,6 +10,9 @@ namespace GameFramework {
     /// デバッグログ
     /// </summary>
     public static class DebugLog {
+        /// <summary>ログを利用する条件となるDefineSymbol</summary>
+        private const string LogConditionalString = "GF_DEBUG";
+
         private static readonly List<ILogger> Loggers = new() { Debug.unityLogger };
 
         /// <summary>
@@ -34,91 +37,91 @@ namespace GameFramework {
             Loggers.Remove(logger);
         }
 
-        [Conditional("GF_DEBUG")]
+        [Conditional(LogConditionalString)]
         public static void Info(string tag, object message, Object context = null) {
             foreach (var logger in Loggers) {
                 logger.Log(LogType.Log, tag, message, context);
             }
         }
 
-        [Conditional("GF_DEBUG")]
+        [Conditional(LogConditionalString)]
         public static void Info(object message, Object context = null) {
             foreach (var logger in Loggers) {
                 logger.Log(LogType.Log, message, context);
             }
         }
 
-        [Conditional("GF_DEBUG")]
+        [Conditional(LogConditionalString)]
         public static void InfoFormat(string format, params object[] args) {
             foreach (var logger in Loggers) {
                 logger.LogFormat(LogType.Log, format, args);
             }
         }
 
-        [Conditional("GF_DEBUG")]
+        [Conditional(LogConditionalString)]
         public static void InfoFormat(Object context, string format, params object[] args) {
             foreach (var logger in Loggers) {
                 logger.LogFormat(LogType.Log, context, format, args);
             }
         }
 
-        [Conditional("GF_DEBUG")]
+        [Conditional(LogConditionalString)]
         public static void Warning(string tag, object message, Object context = null) {
             foreach (var logger in Loggers) {
                 logger.Log(LogType.Warning, tag, message, context);
             }
         }
 
-        [Conditional("GF_DEBUG")]
+        [Conditional(LogConditionalString)]
         public static void Warning(object message, Object context = null) {
             foreach (var logger in Loggers) {
                 logger.Log(LogType.Warning, message, context);
             }
         }
 
-        [Conditional("GF_DEBUG")]
+        [Conditional(LogConditionalString)]
         public static void WarningFormat(string format, params object[] args) {
             foreach (var logger in Loggers) {
                 logger.LogFormat(LogType.Warning, format, args);
             }
         }
 
-        [Conditional("GF_DEBUG")]
+        [Conditional(LogConditionalString)]
         public static void WarningFormat(Object context, string format, params object[] args) {
             foreach (var logger in Loggers) {
                 logger.LogFormat(LogType.Warning, context, format, args);
             }
         }
 
-        [Conditional("GF_DEBUG")]
+        [Conditional(LogConditionalString)]
         public static void Error(string tag, object message, Object context) {
             foreach (var logger in Loggers) {
                 logger.LogError(tag, message, context);
             }
         }
 
-        [Conditional("GF_DEBUG")]
+        [Conditional(LogConditionalString)]
         public static void Error(object message, Object context = null) {
             foreach (var logger in Loggers) {
                 logger.Log(LogType.Error, message, context);
             }
         }
 
-        [Conditional("GF_DEBUG")]
+        [Conditional(LogConditionalString)]
         public static void ErrorFormat(string format, params object[] args) {
             foreach (var logger in Loggers) {
                 logger.LogFormat(LogType.Error, format, args);
             }
         }
 
-        [Conditional("GF_DEBUG")]
+        [Conditional(LogConditionalString)]
         public static void ErrorFormat(Object context, string format, params object[] args) {
             foreach (var logger in Loggers) {
                 logger.LogFormat(LogType.Error, context, format, args);
             }
         }
 
-        [Conditional("GF_DEBUG")]
+        [Conditional(LogConditionalString)]
         public static void Exception(Exception exception, Object context = null) {
             foreach (var logger in Loggers) {
                 logger.LogException(exception, context);
