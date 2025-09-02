fc9cbb1a2e2dff94c320118a0ebd0dd25289439b
DaitokuAmy <daitoku1988@gmail.com>
2025-09-02 17:55:48 +0900

    アウトゲーム画面の調整

---
+++ Assets/UnityUITool/Editor/Window/CopySliceToolWindow.cs
@@ -0,0 +1,72 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityUITool.Editor {
+    /// <summary>
+    /// 9Sliceをコピーするツール
+    /// </summary>
+    public class CopySliceToolWindow : EditorWindow {
+        [SerializeField]
+        private Sprite _sourceSprite;
+        [SerializeField]
+        private Sprite[] _destinationSprites = Array.Empty<Sprite>();
+
+        /// <summary>
+        /// Windowを開く処理
+        /// </summary>
+        [MenuItem("Window/Unity UI Tool/Copy Nine Slice Tool")]
+        private static void Open() {
+            GetWindow<CopySliceToolWindow>("Copy Nine Slice Tool");
+        }
+
+        /// <summary>
+        /// GUI描画
+        /// </summary>
+        private void OnGUI() {
+            var serializedObject = new SerializedObject(this);
+            serializedObject.Update();
+
+            var sourceSpriteProp = serializedObject.FindProperty("_sourceSprite");
+            var destinationSpritesProp = serializedObject.FindProperty("_destinationSprites");
+            EditorGUILayout.PropertyField(sourceSpriteProp, true);
+            EditorGUILayout.PropertyField(destinationSpritesProp, true);
+
+            serializedObject.ApplyModifiedProperties();
+
+            using (new EditorGUI.DisabledScope(!CheckTargetSprite(_sourceSprite, out var sourceImporter) || _destinationSprites.Length <= 0)) {
+                if (GUILayout.Button("Copy")) {
+                    foreach (var destSprite in _destinationSprites) {
+                        if (CheckTargetSprite(destSprite, out var destImporter)) {
+                            destImporter.spriteBorder = sourceImporter.spriteBorder;
+                            EditorUtility.SetDirty(destImporter);
+                            destImporter.SaveAndReimport();
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 対象のSpriteとなるかチェック
+        /// </summary>
+        private bool CheckTargetSprite(Sprite sprite, out TextureImporter importer) {
+            importer = null;
+            
+            if (sprite == null) {
+                return false;
+            }
+            
+            importer = AssetImporter.GetAtPath(AssetDatabase.GetAssetPath(sprite)) as TextureImporter;
+            if (importer == null) {
+                return false;
+            }
+
+            if (importer.spriteImportMode != SpriteImportMode.Single) {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
